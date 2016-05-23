ab0a17f47d117dc73f23e356efe00be5c425f134
br0ther <brotherrrr@gmail.com>
2016-05-23 12:20:18 +0300

    User registration, profile functional tests

--- src/BugTrackBundle/Tests/Controller/UserControllerTest.php
+++ src/BugTrackBundle/Tests/Controller/UserControllerTest.php
@@ -2,7 +2,10 @@
 
 namespace BugTrackBundle\Tests\Controller;
 
+use BugTrackBundle\Entity\User;
+use Doctrine\Common\DataFixtures\ReferenceRepository;
 use Liip\FunctionalTestBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Client;
 
 /**
  * Class UserControllerTest
@@ -10,21 +13,37 @@ use Liip\FunctionalTestBundle\Test\WebTestCase;
  */
 class UserControllerTest extends WebTestCase
 {
+    /**
+     * @var Client
+     */
+    protected $client;
 
     /**
-     * Test for user login
+     * @var ReferenceRepository
+     */
+    protected $fixtures;
+
+    /**
+     * SetUp
+     */
+    public function setUp()
+    {
+        $this->client = $this->makeClient();
+
+        $this->fixtures = $this->loadFixtures([
+            '\BugTrackBundle\DataFixtures\ORM\LoadUserData',
+        ])->getReferenceRepository();
+    }
+    
+    /**
+     * Test user login
      */
     public function testLoginPage()
     {
-        $this->loadFixtures([
-            '\BugTrackBundle\DataFixtures\ORM\LoadUserData',
-        ]);
 
-        $client = $this->makeClient();
-        $client->restart();
-        $crawler = $client->request('GET', '/login');
+        $crawler = $this->client->request('GET', '/login');
 
-        $this->assertEquals(200, $client->getResponse()->getStatusCode());
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
         $this->assertEquals(1, $crawler->filter('form[action="/login_check"]')->count());
         $this->assertEquals(1, $crawler->selectLink('Register')->count());
         $this->assertEquals(1, $crawler->selectButton('Log in')->count());
@@ -34,27 +53,170 @@ class UserControllerTest extends WebTestCase
             '_username' => 'fake',
             '_password' => 'pass',
         ];
-        $crawler = $client->submit($form, $data);
-        $this->assertEquals(302, $client->getResponse()->getStatusCode());
-        $this->assertTrue($client->getResponse()->isRedirect());
-        $crawler = $client->followRedirect();
+        $crawler = $this->client->submit($form, $data);
+        $this->assertEquals(302, $this->client->getResponse()->getStatusCode());
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+        $crawler = $this->client->followRedirect();
         
         $this->assertContains(
-            'Invalid credentials',
-            $client->getResponse()->getContent()
+            'Invalid credentials.',
+            $this->client->getResponse()->getContent()
         );
         
         $data = [
             '_username' => 'manager',
             '_password' => '123456',
         ];
-        $crawler = $client->submit($form, $data);
-        $this->assertTrue($client->getResponse()->isRedirect());
-        $crawler = $client->followRedirect();
+        $crawler = $this->client->submit($form, $data);
+        $this->assertTrue($this->client->getResponse()->isRedirect());
+        $crawler = $this->client->followRedirect();
 
         $this->assertContains(
             'Home',
-            $client->getResponse()->getContent()
+            $this->client->getResponse()->getContent()
+        );
+    }
+
+    /**
+     * Test user register
+     *
+     */
+    public function testRegisterPage()
+    {
+        $crawler = $this->client->request('GET', '/register/');
+
+        $this->assertEquals(200, $this->client->getResponse()->getStatusCode());
+        $this->assertEquals(1, $crawler->filter('form[name="fos_user_registration_form"]')->count());
+        $this->assertEquals(1, $crawler->selectLink('Log in')->count());
+        $this->assertEquals(1, $crawler->selectButton('Register')->count());
+
+        $form = $crawler->filter('form[name="fos_user_registration_form"]')->form();
+
+        $data = [
+            'fos_user_registration_form[email]' => 'd@f',
+            'fos_user_registration_form[username]' => '123456',
+            'fos_user_registration_form[plainPassword][first]' => '123',
+            'fos_user_registration_form[plainPassword][second]' => '1234',
+        ];
+        $crawler = $this->client->submit($form, $data);
+        $content = $this->client->getResponse()->getContent();
+
+        $this->assertContains(
+            'The email is not valid',
+            $content
+        );
+        $this->assertContains(
+            'The entered passwords don&#039;t match',
+            $content
+        );
+
+        $data = [
+            'fos_user_registration_form[email]' => 'admin@test.com',
+            'fos_user_registration_form[username]' => '123456',
+            'fos_user_registration_form[plainPassword][first]' => '123',
+            'fos_user_registration_form[plainPassword][second]' => '123',
+        ];
+        $crawler = $this->client->submit($form, $data);
+        $content = $this->client->getResponse()->getContent();
+
+        $this->assertContains(
+            'The email is already used ',
+            $content
+        );
+
+        $data = [
+            'fos_user_registration_form[email]' => 'admin2@test.com',
+            'fos_user_registration_form[username]' => 'admin2',
+            'fos_user_registration_form[plainPassword][first]' => '123',
+            'fos_user_registration_form[plainPassword][second]' => '123',
+        ];
+        $crawler = $this->client->submit($form, $data);
+
+        $this->assertTrue(
+            $this->client->getResponse()->isRedirect('/register/confirmed')
+        );
+        $crawler = $this->client->followRedirect();
+        $content = $this->client->getResponse()->getContent();
+        $this->assertContains(
+            'Congrats admin2, your account is now activated.',
+            $content
+        );
+    }
+
+    /**
+     * Test user view
+     */
+    public function testUserView()
+    {
+        /** @var User $userAdmin */
+        $userAdmin = $this->fixtures->getReference('userAdmin');
+        $this->loginAs($userAdmin, 'main');
+        $loggedClient = static::makeClient();
+
+        $crawler = $loggedClient->request('GET', '/user/view/' . $userAdmin->getId());
+
+        $this->assertEquals(1, $crawler->selectLink('Edit')->count());
+        $this->assertEquals(0, $crawler->filter('form')->count());
+        $content = $loggedClient->getResponse()->getContent();
+
+        $this->assertContains(
+            'Andy Admin',
+            $content
+        );
+        $this->assertContains(
+            'Administrator',
+            $content
+        );
+    }
+
+    /**
+     * Test user edit
+     */
+    public function testUserEdit()
+    {
+        /** @var User $userAdmin */
+        $userAdmin = $this->fixtures->getReference('userAdmin');
+        $this->loginAs($userAdmin, 'main');
+        $loggedClient = static::makeClient();
+        
+        $crawler = $loggedClient->request('GET', '/user/edit/' . $userAdmin->getId());
+        $this->assertEquals(1, $crawler->selectButton('save')->count());
+        $this->assertEquals(1, $crawler->selectButton('cancel')->count());
+        $this->assertEquals(
+            1,
+            $crawler->filterXPath('//*[@id="user_form_roles"]/option[@value="ROLE_ADMIN" and @selected="selected"]')
+                ->count()
+        );
+        $form = $crawler->filter('form[name=user_form]')->form();
+        
+        $data = [
+            'user_form[email]' => 'changed_admin@test.com',
+            'user_form[username]' => 'changed_name',
+            'user_form[fullName]' => 'Changed full name',
+            'user_form[roles]' => ['ROLE_MANAGER', 'ROLE_ADMIN'],
+        ];
+
+        $crawler = $loggedClient->submit($form, $data);
+        $this->assertTrue($loggedClient->getResponse()->isRedirect());
+
+        $crawler = $loggedClient->followRedirect();
+        $content = $loggedClient->getResponse()->getContent();
+
+        $this->assertContains(
+            'changed_admin@test.com',
+            $content
+        );
+        $this->assertContains(
+            'changed_name',
+            $content
+        );
+        $this->assertContains(
+            'Changed full name',
+            $content
+        );
+        $this->assertContains(
+            'Administrator, Manager',
+            $content
         );
     }
 }
